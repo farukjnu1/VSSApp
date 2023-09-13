c5e655eca2350ec20521f4c42fa09b737e6fd889
mahsin-ahmed <mahsin.cse@gmail.com>
2023-09-13 12:08:56 +0600

    commit for pulling from Raju

---
+++ VSS.API/Controllers/RoleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VSS.API.BL.System;
+using VSS.API.DA.ViewModels.System;
+
+namespace VSS.API.Controllers
+{
+    public class RoleController : ApiController
+    {
+        // GET: api/Role
+        public IEnumerable<RoleVM> Get()
+        {
+            RoleBL _BL = new RoleBL();
+            return _BL.get();
+        }
+
+        // GET: api/Role/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Role
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Role/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Role/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ VSS.API/DA/ViewModels/System/RoleVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VSS.API.DA.ViewModels.System
+{
+    public class RoleVM
+    {
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}

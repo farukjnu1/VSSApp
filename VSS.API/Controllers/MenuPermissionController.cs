6b2bda61c1e345d30fbe272eedbc67ee35231d75
mahsin-ahmed <mahsin.cse@gmail.com>
2023-09-14 11:57:48 +0600

    Menu Permission Done

---
+++ VSS.API/Controllers/MenuPermissionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VSS.API.BL.System;
+using VSS.API.DA.ViewModels.System;
+
+namespace VSS.API.Controllers
+{
+    public class MenuPermissionController : ApiController
+    {
+        // GET: api/MenuPermission
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MenuPermission/5
+        [HttpGet]
+        [Route("api/MenuPermission/GetMenuPermission")]
+        public List<MenuPermissionVM> GetMenuPermission(int RoleId)
+        {
+            return new MenuPermissionBL().GetMenuPermission(RoleId);
+        }
+
+        // POST: api/MenuPermission
+        public bool Post([FromBody] List<MenuPermissionVM> Model)
+        {
+            MenuPermissionBL _BL = new MenuPermissionBL();
+            return _BL.add(Model);
+        }
+
+        // PUT: api/MenuPermission/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/MenuPermission/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ VSS.API/DA/ViewModels/System/MenuPermissionVM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VSS.API.DA.EF.VssDb;
+
+namespace VSS.API.DA.ViewModels.System
+{
+    public class MenuPermissionVM
+    {
+        public int? MenuId { get; set; }
+
+        public int? ModuleId { get; set; }
+
+        public string MenuCode { get; set; }
+
+        public string MenuName { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public bool? IsSubParent { get; set; }
+
+        public int? SubParentId { get; set; }
+
+        public string MenuIcon { get; set; }
+
+        public string MenuPath { get; set; }
+
+        public int MenuSequence { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public int? RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public string ModuleName { get; set; }
+
+        public bool? CanView { get; set; }
+
+        public bool? CanCreate { get; set; }
+        
+        public bool? CanEdit { get; set; }
+
+        public bool? CanDelete { get; set; }
+    }
+}

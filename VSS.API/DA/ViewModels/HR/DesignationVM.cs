ca37c535fbe2367373d7cf5221546a844400a31b
mahsin-ahmed <mahsin.cse@gmail.com>
2023-11-08 10:33:54 +0600

    Designation Done

---
+++ VSS.API/DA/ViewModels/HR/DesignationVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VSS.API.DA.ViewModels.Common;
+
+namespace VSS.API.DA.ViewModels.HR
+{
+    public class DesignationVM: Pager
+    {
+        public int DesignateId { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(10)]
+        public string Short { get; set; }
+    }
+}

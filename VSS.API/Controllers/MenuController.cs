b5b1ae0ccc276373080ea066b07730679dcd07a1
mahsin-ahmed <mahsin.cse@gmail.com>
2023-11-09 11:31:57 +0600

    Menu and Module Read done

---
+++ VSS.API/Controllers/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VSS.API.BL.HR;
+using VSS.API.BL.System;
+using VSS.API.DA.ViewModels.HR;
+using VSS.API.DA.ViewModels.System;
+
+namespace VSS.API.Controllers
+{
+    public class MenuController : ApiController
+    {
+        // GET: api/Menu
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public IEnumerable<MenuVM> Get([FromUri] string phone, int pi = 0, int ps = 10)
+        {
+            MenuBL _BL = new MenuBL();
+            return _BL.Get(phone, pi, ps);
+        }
+
+        // GET: api/Menu/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Menu
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Menu/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Menu/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ VSS.API/DA/ViewModels/System/MenuVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VSS.API.DA.ViewModels.Common;
+
+namespace VSS.API.DA.ViewModels.System
+{
+    public class MenuVM : Pager
+    {
+        public int MenuId { get; set; }
+
+        public int? ModuleId { get; set; }
+
+        [StringLength(50)]
+        public string MenuCode { get; set; }
+
+        [StringLength(50)]
+        public string MenuName { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public bool? IsSubParent { get; set; }
+
+        public int? SubParentId { get; set; }
+
+        [StringLength(50)]
+        public string MenuIcon { get; set; }
+
+        [StringLength(250)]
+        public string MenuPath { get; set; }
+
+        public int? MenuSequence { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}

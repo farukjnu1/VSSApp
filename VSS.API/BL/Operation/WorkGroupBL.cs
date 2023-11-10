7504c9cfcf8f285f6efc912043c766f7ea564fe3
mahsin-ahmed <mahsin.cse@gmail.com>
2023-11-10 11:21:56 +0600

    Work Group CRUD done

---
+++ VSS.API/BL/Operation/WorkGroupBL.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VSS.API.DA.EF.VssDb;
+using VSS.API.DA.ViewModels.Operation;
+using VSS.API.DA.ViewModels.System;
+
+namespace VSS.API.BL.Operation
+{
+    public class WorkGroupBL
+    {
+        ModelVssDb _vssDb = new ModelVssDb();
+        public IEnumerable<WorkGroupVM> Get(string phone, int pageIndex, int pageSize)
+        {
+            int nRow = _vssDb.WorkGroups.Count();
+            var listWorkGroup = _vssDb.WorkGroups
+                .Select(x => new WorkGroupVM
+                {
+                    WgId = x.WgId,
+                    WgName = x.WgName,
+
+                    PageIndex = pageIndex,
+                    PageSize = pageSize,
+                    RowCount = nRow
+                })
+                .OrderBy(s => s.WgId)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToList();
+            return listWorkGroup;
+        }
+
+        public bool Add(WorkGroup model)
+        {
+            try
+            {
+                model.WgId = GetNewId();
+                model.WgName = model.WgName;
+                _vssDb.WorkGroups.Add(model);
+                _vssDb.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(WorkGroup model)
+        {
+            try
+            {
+                var oWorkGroup = _vssDb.WorkGroups
+                 .Where(x => x.WgId == model.WgId)
+                 .FirstOrDefault();
+                if (oWorkGroup != null)
+                {
+                    oWorkGroup.WgId = model.WgId;
+                    oWorkGroup.WgName = model.WgName;
+
+                    _vssDb.SaveChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public bool Remove(int id)
+        {
+            try
+            {
+                var selectedWorkGroup = _vssDb.WorkGroups
+                 .Where(x => x.WgId == id)
+                 .FirstOrDefault();
+                if (selectedWorkGroup != null)
+                {
+                    _vssDb.WorkGroups.Remove(selectedWorkGroup);
+                    _vssDb.SaveChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private int GetNewId()
+        {
+            try
+            {
+                var Id = Convert.ToInt32(_vssDb.WorkGroups.Max(x => x.WgId)) + 1;
+                return Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}

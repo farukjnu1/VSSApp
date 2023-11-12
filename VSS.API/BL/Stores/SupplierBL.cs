99441736eb5f8b33c45044d7e5b95db0aa4d3e0b
mahsin-ahmed <mahsin.cse@gmail.com>
2023-11-12 16:08:25 +0600

    Supplier CRU done but delete

--- VSS.API/BL/Stores/SupplierBL.cs
+++ VSS.API/BL/Stores/SupplierBL.cs
@@ -1,11 +1,118 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
+using VSS.API.DA.ADO;
+using VSS.API.DA.EF.VssDb;
+using VSS.DA.ADO;
+using VSS.DA.ViewModels.Operation;
 
 namespace VSS.API.BL.Stores
 {
+
     public class SupplierBL
     {
+        ModelVssDb _vssDb = new ModelVssDb();
+        List<ClientVM> listClient = null;
+        ClientVM oClient = null;
+        private IGenericFactory<ClientVM> Generic_Client = null;
+
+        public IEnumerable<ClientVM> Get(string phone, int pageIndex, int pageSize)
+        {
+            string vssDb = ConfigurationManager.ConnectionStrings["VssDb"].ConnectionString;
+            Generic_Client = new GenericFactory<ClientVM>();
+            var oHashTable = new Hashtable()
+            {
+                { "PageIndex", pageIndex },
+                { "PageSize", pageSize },
+                { "phone", phone }
+            };
+            return Generic_Client.ExecuteCommandList(CommandType.StoredProcedure, StoredProcedure.sp_GetClient, oHashTable, vssDb);
+        }
+
+        public bool Add(BusinessPartner model)
+        {
+            try
+            {
+                model.BpId = GetNewId();
+                model.BpTypeId = 2;
+                model.CreateDate = DateTime.Now;
+                _vssDb.BusinessPartners.Add(model);
+                _vssDb.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(BusinessPartner model)
+        {
+            try
+            {
+                var oBP = _vssDb.BusinessPartners
+                 .Where(x => x.BpTypeId == 1 && x.BpId == model.BpId)
+                 .FirstOrDefault();
+                if (oBP != null)
+                {
+                    oBP.BpTypeId = model.BpTypeId;
+                    oBP.Name = model.Name;
+                    oBP.Phone = model.Phone;
+                    oBP.Email = model.Email;
+                    oBP.Address = model.Address;
+                    oBP.ContactPerson = model.ContactPerson;
+                    oBP.ContactPersonNo = model.ContactPersonNo;
+                    oBP.ClientInfo = model.ClientInfo;
+                    oBP.MembershipNo = model.MembershipNo;
+                    oBP.UpdateBy = model.UpdateBy;
+                    oBP.UpdateDate = DateTime.Now;
+                    _vssDb.SaveChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public bool Remove(int id)
+        {
+            try
+            {
+                var oBP = _vssDb.BusinessPartners
+                 .Where(x => x.BpTypeId == 1 && x.BpId == id)
+                 .FirstOrDefault();
+                if (oBP != null)
+                {
+                    _vssDb.BusinessPartners.Remove(oBP);
+                    _vssDb.SaveChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private int GetNewId()
+        {
+            try
+            {
+                var nid = Convert.ToInt32(_vssDb.BusinessPartners.Max(x => x.BpId)) + 1;
+                return nid;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
     }
 }
--- VSS.API/Controllers/SupplierController.cs
+++ VSS.API/Controllers/SupplierController.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using VSS.API.BL.Stores;
+using VSS.API.DA.EF.VssDb;
+using VSS.BL.Operation;
+using VSS.DA.ViewModels.Operation;
 
 namespace VSS.API.Controllers
 {
     public class SupplierController : ApiController
     {
         // GET: api/Supplier
-        public IEnumerable<string> Get()
+        public IEnumerable<ClientVM> Get([FromUri] string phone, int pi = 0, int ps = 10)
         {
-            return new string[] { "value1", "value2" };
+            SupplierBL _BL = new SupplierBL();
+            return _BL.Get(phone, pi, ps);
         }
 
         // GET: api/Supplier/5
@@ -22,18 +27,24 @@ namespace VSS.API.Controllers
         }
 
         // POST: api/Supplier
-        public void Post([FromBody]string value)
+        public bool Post([FromBody] BusinessPartner model)
         {
+            SupplierBL _BL = new SupplierBL();
+            return _BL.Add(model);
         }
 
         // PUT: api/Supplier/5
-        public void Put(int id, [FromBody]string value)
+        public bool Put([FromBody] BusinessPartner model)
         {
+            SupplierBL _BL = new SupplierBL();
+            return _BL.Update(model);
         }
 
         // DELETE: api/Supplier/5
-        public void Delete(int id)
+        public bool Delete(int id)
         {
+            SupplierBL _BL = new SupplierBL();
+            return _BL.Remove(id);
         }
     }
 }
